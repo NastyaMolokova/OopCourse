4dd3e2eb2366932029d3c574ea97e9d9096df4c5
NastyaMolokova <nastya.molokowa@yandex.ru>
2025-09-10 12:30:15 +0300

    Обновление List

--- List/src/ru/academits/molokova/list_main/Main.java
+++ List/src/ru/academits/molokova/list_main/Main.java
@@ -10,32 +10,32 @@ public class Main {
         list.addFirst(6);
         list.add(1, 5);
 
-        System.out.println(list);
+        System.out.println("Список " + list);
 
-        System.out.println("Размер: " + list.size());
-        System.out.println("Первый: " + list.getFirst());
+        System.out.println("Размер списка: " + list.size());
+        System.out.println("Первый элемент списка: " + list.getFirst());
 
-        System.out.println("Замена: " + list.set(1, 6));
-        System.out.println("После замены: " + list);
+        System.out.println("Замена элемента: " + list.set(1, 6));
+        System.out.println("Список после замены: " + list);
 
-        System.out.println("Значение по индексу '0': " + list.get(0));
+        System.out.println("Значение элемента по индексу '0': " + list.get(0));
 
-        System.out.println("Удалён по индексу '2': " + list.remove(2));
-        System.out.println("После удаления: " + list);
+        System.out.println("Удалён элемент по индексу '2': " + list.remove(2));
+        System.out.println("Список после удаления: " + list);
 
-        System.out.println("Удалён первый элемент: " + list.removeFirst());
-        System.out.println("После удаления: " + list);
+        System.out.println("Удалён первый элемент списка: " + list.removeFirst());
+        System.out.println("Список после удаления: " + list);
 
         list.addFirst(9);
-        System.out.println("После добавления: " + list);
+        System.out.println("Список после добавления в начало: " + list);
 
         list.reverse();
-        System.out.println("После разворота: " + list);
+        System.out.println("Список после разворота: " + list);
 
         SinglyLinkedList<Integer> copy = list.copy();
-        System.out.println("Копия: " + copy);
+        System.out.println("Копия списка: " + copy);
 
-        System.out.println("Удаление по значению '5': " + copy.removeByValue(5));
-        System.out.println("Копия после удаления: " + copy);
+        System.out.println("Удаление элемента по значению '5': " + copy.removeByValue(5));
+        System.out.println("Копия списка после удаления: " + copy);
     }
 }
