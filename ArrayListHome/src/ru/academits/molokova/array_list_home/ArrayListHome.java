94000997ac92d25b12d8e650092591e13c5d1499
NastyaMolokova <nastya.molokowa@yandex.ru>
2025-09-14 11:37:00 +0300

    Обновление ArrayListHome

--- ArrayListHome/src/ru/academits/molokova/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/molokova/array_list_home/ArrayListHome.java
@@ -1,56 +1,55 @@
-package ru.academits.molokova.array_main;
+package ru.academits.molokova.array_list_home;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayListHome {
-    public static ArrayList<Integer> getOddArrayList(ArrayList<Integer> numbersList) {
+    public static void removeEvenNumbers(ArrayList<Integer> numbersList) {
         for (int i = numbersList.size() - 1; i >= 0; i--) {
             if (numbersList.get(i) % 2 == 0) {
                 numbersList.remove(i);
             }
         }
-
-        return numbersList;
     }
 
-    public static ArrayList<Integer> removeDuplication(ArrayList<Integer> numbersList) {
-        ArrayList<Integer> newNumbersList = new ArrayList<>(numbersList.size());
+    public static <T> ArrayList<T> getUniqueElementsList(ArrayList<T> list) {
+        ArrayList<T> result = new ArrayList<>(list.size());
 
-        for (Integer number : numbersList) {
-            if (!newNumbersList.contains(number)) {
-                newNumbersList.add(number);
+        for (T element : list) {
+            if (!result.contains(element)) {
+                result.add(element);
             }
         }
 
-        return newNumbersList;
+        return result;
     }
 
     public static void main(String[] args) {
-        ArrayList<String> lines = new ArrayList<>();
-
         try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
+            ArrayList<String> lines = new ArrayList<>();
             String line;
 
             while ((line = reader.readLine()) != null) {
                 lines.add(line);
             }
+
+            System.out.println("Содержимое файла: " + lines);
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден!");
         } catch (IOException e) {
             System.err.println("Ошибка при чтении файла: " + e.getMessage());
-            return;
         }
 
-        System.out.println("Содержимое файла: " + lines);
-
         ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 5, 8, 7, 3, 2, 6, 9, 4, 5, 3, 9));
-
         System.out.println("Исходный список чисел: " + numbersList);
 
-        System.out.println("Список после удаления четных чисел: " + getOddArrayList(numbersList));
+        removeEvenNumbers(numbersList);
 
-        System.out.println("Новый список без повторяющихся элементов: " + removeDuplication(numbersList));
+        System.out.println("Список после удаления четных чисел: " + numbersList);
+        System.out.println("Новый список без повторяющихся элементов: " + getUniqueElementsList(numbersList));
     }
 }
